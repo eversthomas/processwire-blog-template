2899b6a6b30ae12726c2607f77a9c33c092eb981
Thomas <evers.thomas@gmail.com>
2024-03-10 00:16:11 +0100

    Add files via upload

---
+++ install-version/install/info.php
@@ -0,0 +1,7 @@
+<?php namespace ProcessWire;
+if(!defined("PROCESSWIRE_INSTALL")) die();
+$info = array(
+	'title' => "unstyled blog template to install", 
+	'summary' => "", 
+	'screenshot' => ""
+);
